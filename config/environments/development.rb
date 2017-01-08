466d94c4cdeeac07a21b196eac2ae2cca2e9e47d
faralbrt <albert.farhi5@gmail.com>
2017-01-08 18:22:07 -0500

    add searches controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.assets.enabled = false
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
